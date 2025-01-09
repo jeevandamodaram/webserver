09c96f198ad6313a94979f937237663b22fecfcd
Jeevan Kumar <51814064+jeevandamodaram@users.noreply.github.com>
2025-01-09 13:49:37 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
-<?php  
-    // This will redirect the user to the new location  
-    header('Location: https://kanchiuniv.ac.in/');  
-      
-    //The below code will not execute after header  
-    exit;  
-?>  
+<?php
+   header('Location: https://kanchiuniv.ac.in/');  
+   exit;  
+?>
+
+
+<form align="center" name="STUDENT" method="GET" action="page2.php">
+
+NAME: <input type="text" name="name" required></>
+<br><br>
+AGE: <input type="text" name="age" required></>
+<br><br>
+EMAIL: <input type="email" name="email" required></>
+<br><br>
+
+<input type="SUBMIT" Value="REGISTER"></>
+
+</form>
