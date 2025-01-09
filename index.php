a2787a149cf3053cc52ddeaed6fb2b23d593723a
Jeevan Kumar <51814064+jeevandamodaram@users.noreply.github.com>
2025-01-09 14:00:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,3 @@
    exit;  
 ?>
 
-
-<form align="center" name="STUDENT" method="GET" action="page2.php">
-
-NAME: <input type="text" name="name" required></>
-<br><br>
-AGE: <input type="text" name="age" required></>
-<br><br>
-EMAIL: <input type="email" name="email" required></>
-<br><br>
-
-<input type="SUBMIT" Value="REGISTER"></>
-
-</form>
