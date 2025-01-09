194acd569faa7872aeed143bfc12fbbc1320acc0
Jeevan Kumar <51814064+jeevandamodaram@users.noreply.github.com>
2025-01-09 13:42:11 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php  
+    // This will redirect the user to the new location  
+    header('Location: https://kanchiuniv.ac.in/');  
+      
+    //The below code will not execute after header  
+    exit;  
+?>  
